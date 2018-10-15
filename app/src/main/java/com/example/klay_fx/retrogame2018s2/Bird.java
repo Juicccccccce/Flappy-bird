2c28b7498261e7a3c04796ba5db8817433ec27e2
Xuan Feng <u6234544@anu.edu.au>
2018-10-15 19:40:40 +1100

    draw Bird

--- app/src/main/java/com/example/klay_fx/retrogame2018s2/Bird.java
+++ app/src/main/java/com/example/klay_fx/retrogame2018s2/Bird.java
@@ -1,4 +1,23 @@
 package com.example.klay_fx.retrogame2018s2;
 
-public class Bird {
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Bird extends Item {
+    public static final float STARTX= (1.0f / 2.0f);
+    public static final float STARTY= (1.0f / 2.0f);
+
+    public Bird() {
+        pos = new Pos(STARTX,STARTY);
+    }
+
+    // draw the Bird
+    public void draw(Canvas c , Paint p) {
+        int w = c.getWidth();
+        int h = c.getHeight();
+
+        float xc = pos.x * w;
+        float yc = pos.y * h;
+//        c.drawBitmap();      draw the picture of the bird.
+    }
 }

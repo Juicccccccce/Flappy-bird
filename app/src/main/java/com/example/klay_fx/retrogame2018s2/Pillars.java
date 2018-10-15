0b081e467d92decb6fc7304e9431e8e0acc138e1
Xuan Feng <u6234544@anu.edu.au>
2018-10-15 19:00:15 +1100

    create game model

--- app/src/main/java/com/example/klay_fx/retrogame2018s2/Pillars.java
+++ app/src/main/java/com/example/klay_fx/retrogame2018s2/Pillars.java
@@ -1,4 +1,25 @@
 package com.example.klay_fx.retrogame2018s2;
 
-public class Pillars {
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Pillars extends ArrayList<Pillar> {
+
+    public static final float PILLARSTEP = 0.03f;
+
+    public void step() {
+        for (Pillar b : this) b.pos.x += PILLARSTEP;
+        Iterator<Pillar> bi = this.iterator();
+        while (bi.hasNext()) {
+            Pillar b = bi.next();
+            if (b.pos.x > Game.MAXXY) bi.remove();
+        }
+    }
+
+    public void draw(Canvas canvas, Paint paint) {
+        for (Pillar b : this) b.draw(canvas, paint);
+    }
 }

be1437cd638614a2640190dce9012cd1ddd1a092
Xuan Feng <u6234544@anu.edu.au>
2018-10-16 16:39:09 +1100

    random pillar height

--- app/src/main/java/com/example/klay_fx/retrogame2018s2/Pillar.java
+++ app/src/main/java/com/example/klay_fx/retrogame2018s2/Pillar.java
@@ -3,6 +3,8 @@ package com.example.klay_fx.retrogame2018s2;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.Random;
+
 
 /**
  * @author Xuan Feng
@@ -18,14 +20,19 @@ public class Pillar extends Item {
 
     // draw the Pillar
     public void draw(Canvas c , Paint p) {
+
         int w = c.getWidth();
         int h = c.getHeight();
 
+        Random r = new Random();
         float xc = pos.x * w;
-        float left = xc + PILLARWIDTH/2;
+        float left = xc - PILLARWIDTH/2;
         float right = xc + PILLARWIDTH/2;
-        float whitetop = WHITETOP * h;
-        float whitebottom = WHITEBOTTOM * h;
+
+        float yc = r.nextFloat()/3 + WHITETOP ;
+        float whitetop = yc - 0.1f;
+        float whitebottom = yc + 0.1f;
+
         c.drawRect(left,0.0f,right,whitetop,p);
         c.drawRect(left,0.0f,right,whitebottom,p);
     }

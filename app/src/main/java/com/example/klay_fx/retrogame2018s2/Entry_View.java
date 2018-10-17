9792f3a69b22fa06a7f7628270049d63b8b64038
u6105656 <9Lovemyself>
2018-10-17 22:10:48 +1100

    update entry_view

---
+++ app/src/main/java/com/example/klay_fx/retrogame2018s2/Entry_View.java
@@ -0,0 +1,25 @@
+package com.example.klay_fx.retrogame2018s2;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Entry_View extends View{
+        Canvas c =new Canvas();
+
+    public Entry_View(Context context) {
+        super(context);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+}
--- app/src/main/java/com/example/klay_fx/retrogame2018s2/GameActivity.java
+++ app/src/main/java/com/example/klay_fx/retrogame2018s2/GameActivity.java
@@ -1,9 +1,11 @@
 package com.example.klay_fx.retrogame2018s2;
 
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -28,9 +30,29 @@ public class GameActivity extends AppCompatActivity implements GameOver {
         gameView.registerGameOver(this);
     }
 
+//    final ImageView backgroundOne = (ImageView) findViewById(R.id.ground_One);
+//    final ImageView backgroundTwo = (ImageView) findViewById(R.id.ground_Two);
+//
+//    final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+//    animator.setRepeatCount(ValueAnimator.INFINITE);
+//    animator.setInterpolator(new LinearInterpolator());
+//    animator.setDuration(10000L);
+//    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//        @Override
+//        public void onAnimationUpdate(ValueAnimator animation) {
+//            final float progress = (float) animation.getAnimatedValue();
+//            final float width = backgroundOne.getWidth();
+//            final float translationX = width * progress;
+//            backgroundOne.setTranslationX(translationX);
+//            backgroundTwo.setTranslationX(translationX - width);
+//        }
+//    });
+//animator.start();
+
     @Override
     public void gameOver() {
         setResult(RESULT_OK);
         finish();
     }
+
 }
